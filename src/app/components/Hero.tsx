3481bb7cc1a6f66c2254fbfbcb1fc055d7bcb3d8
roniadiatmoko <roniadiatmoko@student.uns.ac.id>
2025-06-20 15:40:43 +0700

    fix: buton hero

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -10,9 +10,17 @@ export default function Hero(){
                         <span className="text-purple-600 font-bold">Roni Adiatmoko.</span>
                     </h1>
                     <p className="text-lg text-zinc-900 dark:text-zinc-400">
-                        <span className="font-bold text-purple-300">Developers</span><br/>
+                        <span className="font-bold text-purple-400">Developers</span><br/>
                         I'm a passionate developer who loves crafting clean and efficient code. Welcome to my page!
                     </p>
+                    <div className="flex">
+                        <Link href="/projects" className="px-6 py-2 bg-purple-400 rounded-full font-medium">
+                            My Projects
+                        </Link>
+                        <Link href="/contact" className="ml-5 px-6 py-2 bg-gray-400 rounded-full font-medium">
+                            Contact Me
+                        </Link>
+                    </div>
                 </div>
                 
                 <div className="flex justify-center md:justify-end">
