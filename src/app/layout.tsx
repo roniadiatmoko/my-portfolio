2dc5cc648741b3aa4b2ef15aff4cbf8cea5eb51b
roniadiatmoko <roniadiatmoko@student.uns.ac.id>
2025-06-21 10:33:14 +0700

    fix: dashed bg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import '../styles/globals.css'
-import {ReactNode} from 'react'
+import { ReactNode } from 'react'
 import Navbar from './components/Navbar'
 import Link from 'next/link'
 
@@ -8,11 +8,26 @@ export const metadata = {
   description: 'My Portfolio | Next JS 15 | React 19'
 }
 
-export default function RootLayout({children}: {children: ReactNode}){
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="id" className="dark">
       <body className="transition-colors duration-300 bg-white text-black dark:bg-zinc-900 dark:text-white">
-          <Navbar />
+        {/* Background grid untuk Light Mode */}
+        <div
+          className="absolute inset-0 z-0 opacity-10 dark:hidden"
+          style={{
+            backgroundImage: `url("data:image/svg+xml,%3Csvg width='50' height='50' viewBox='0 0 40 40' xmlns='http://www.w3.org/2000/svg'%3E%3Crect x='0.5' y='0.5' width='39' height='39' fill='none' stroke='%237C3AED' stroke-dasharray='4 4'/%3E%3C/svg%3E")`,
+          }}
+        />
+
+        {/* Background grid untuk Dark Mode */}
+        <div
+          className="absolute inset-0 z-0 opacity-10 hidden dark:block"
+          style={{
+            backgroundImage: `url("data:image/svg+xml,%3Csvg width='50' height='50' viewBox='0 0 40 40' xmlns='http://www.w3.org/2000/svg'%3E%3Crect x='0.5' y='0.5' width='39' height='39' fill='none' stroke='%23C084FC' stroke-dasharray='4 4'/%3E%3C/svg%3E")`,
+          }}
+        />
+        <Navbar />
         <main>{children}</main>
       </body>
     </html>
